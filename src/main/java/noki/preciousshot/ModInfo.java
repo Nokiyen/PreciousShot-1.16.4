9eca925917cbd44326e481e2eba07b59475dec1e
Nokyen <nokinoki.1928@gmail.com>
2021-01-29 22:00:58 +0900

    no message

--- src/main/java/noki/preciousshot/ModInfo.java
+++ src/main/java/noki/preciousshot/ModInfo.java
@@ -13,7 +13,7 @@ public class ModInfo {
 	//define member variables.
 	//*****	
 	public static final String ID = "preciousshot";
-	public static final String NAME = "Precious Shot";
+//	public static final String NAME = "Precious Shot";
 //	public static final String VERSION = "1.6.0";
 //	public static final String CHANNEL = ID;
 //	public static final String DEPENDENCIES = "required-after:forge@[1.16.4-35.0.0,)";
